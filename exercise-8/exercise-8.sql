b10c4c350fdead904cd6f9fb7124ad97e1e72d68
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-23 15:21:19 -0700

    passing through nine

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,25 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+SELECT
+	category.name,
+    COUNT(*),
+    RANK() OVER (ORDER BY COUNT(*) DESC)
+FROM
+	category
+INNER JOIN
+	film_category
+ON
+	category.category_id = film_category.category_id
+INNER JOIN
+	film
+ON
+	film.film_id = film_category.film_id
+INNER JOIN
+	inventory
+ON 
+	inventory.film_id = film.film_id
+INNER JOIN
+	rental
+ON
+	rental.inventory_id = inventory.inventory_id
+GROUP BY category.name
